15afef9eec2e9d2feaa7177126e7827ae8fa79b6
wopqw <wopqw@yandex.ru>
2016-08-31 02:12:55 +0300

    task01

---
+++ module01/src/java/task01/logic/Logic.java
@@ -0,0 +1,11 @@
+package task01.logic;
+
+/**
+ * Created by woqpw on 29.08.16.
+ */
+public class Logic {
+
+    public String method(){
+        return "I am string in Logic";
+    }
+}
---
+++ module01/src/java/task01/main/Main.java
@@ -0,0 +1,15 @@
+package task01.main;
+
+
+import task01.logic.Logic;
+
+/**
+ * Created by woqpw on 29.08.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
