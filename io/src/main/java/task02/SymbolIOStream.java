3e21c5801fb0dbea8c8e4a4515906e5260aad1e3
wopqw <wopqw@yandex.ru>
2016-10-11 22:58:16 +0300

    io-task02

---
+++ io/src/main/java/task02/SymbolIOStream.java
@@ -0,0 +1,63 @@
+package task02;
+
+import lombok.SneakyThrows;
+import task01.JavaKeywords;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Optional;
+
+/**
+ * Created by wopqw on 11.10.16.
+ */
+public interface SymbolIOStream {
+
+    @SneakyThrows
+    static String readFromFile(String filePath){
+
+        StringBuilder sBuilder = new StringBuilder();
+
+        int data;
+
+        try(FileReader fReader =
+                    new FileReader(filePath)){
+
+            while ((data = fReader.read()) != -1){
+                sBuilder.append((char) data);
+            }
+        }
+        return sBuilder.toString();
+    }
+
+
+    @SneakyThrows
+    static void writeToFile(String text, String filePath){
+
+        try(OutputStreamWriter osw = new OutputStreamWriter(
+                new FileOutputStream(filePath))){
+
+            osw.write(text);
+        }
+    }
+
+    static HashMap<String,Integer> processString(String text){
+
+        HashMap<String, Integer> answerMap = new HashMap();
+
+        String[] keywords = Arrays.stream(text.replaceAll("\n"," ").split(" "))
+                .filter(JavaKeywords::contains)
+                .toArray(String[]::new);
+
+        Arrays.stream(keywords)
+                .forEach(w -> {
+                    Integer old = Optional.ofNullable(answerMap.get(w)).orElse(0);
+                    answerMap.put(w,old+1);
+                });
+
+
+        return answerMap;
+    }
+}
---
+++ io/src/test/java/task02/SymbolIOStreamTest.java
@@ -0,0 +1,50 @@
+package task02;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by wopqw on 11.10.16.
+ */
+public class SymbolIOStreamTest {
+
+    private static final String filePath1 = "src/test/resources/hello.txt";
+    private static final String filePath2 = "src/test/resources/writeHello2";
+    private static final String filePath3 = "src/main/java/task01/ByteIOStream.java";
+
+
+    @Test
+    public void readFromFile() throws Exception {
+        assertThat(SymbolIOStream.readFromFile(filePath1),is("Привет мир!"));
+    }
+
+    @Test
+    public void writeToFile() throws Exception {
+
+        String text = "I wrote it in the file again\nIn three lines\nnow";
+        SymbolIOStream.writeToFile(text,filePath2);
+        assertThat(SymbolIOStream.readFromFile(filePath2),is(text));
+
+    }
+
+    @Test
+    public void countKeywordAppereance(){
+
+        String fileInString = SymbolIOStream.readFromFile(filePath3);
+        HashMap<String, Integer> map = SymbolIOStream.processString(fileInString);
+        assertThat(map.get("static"),is(3));
+    }
+
+    @Test
+    public void replacementTest(){
+
+        String bigString = "It is\nnew String\nand it will\nkeeewwwqqee ewqwe";
+        assertThat(bigString.replaceAll("\n"," "),
+                is("It is new String and it will keeewwwqqee ewqwe"));
+    }
+
+}
