0dae44255678e396e370cf9ab0acfa0b63eb43f7
wopqw <wopqw@yandex.ru>
2016-10-16 19:57:07 +0300

    exceptions-task02

---
+++ exceptions/src/main/java/task02/NoSuchPropertyException.java
@@ -0,0 +1,12 @@
+package task02;
+
+/**
+ * Created by wopqw on 16.10.16.
+ */
+public class NoSuchPropertyException extends Exception {
+
+    public NoSuchPropertyException(String message){
+
+        super(message);
+    }
+}
